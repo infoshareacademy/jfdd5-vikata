af528a6b0dbc89aead9d7b60608bbf3c55521fa0
Alwox <alanwolejko@gmail.com>
2016-11-30 21:39:59 +0100

    wstępny grid i jego poruszanie

--- js/grid.js
+++ js/grid.js
@@ -1,2 +1,44 @@
 // 1. tabela która przechowuje stan fieldow
 // 2. funkcja rysujaca divy co chwile na podstawie tabeli
+
+var board = [
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,0,1,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,1,0,0],
+    [0,1,1,1,0],
+    [0,1,0,1,0],
+    [0,1,0,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,0,1,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,1,0,1,0],
+    [0,1,1,1,0]
+];
+
+var t=15;
+
+function draw_board(){
+    for(var i=t;i<(t+5);i++){
+        for(var j=0;j<5;j++){
+            if(board[i][j]===0)
+                $('.board').append('<div class="board__field board__field_grass"></div>');
+            else
+                $('.board').append('<div class="board__field board__field_road"></div>');
+        }
+    }
+}
+
+setInterval(function(){
+    $('.board').empty();
+    draw_board();
+    t--;
+}, 500);
