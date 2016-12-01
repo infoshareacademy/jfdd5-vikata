f4b93c64d9f4af6c3c06c4707dac1873395650ae
Alwox <alanwolejko@gmail.com>
2016-12-01 16:17:00 +0100

    poruszanie kwadratem

--- js/grid.js
+++ js/grid.js
@@ -19,13 +19,12 @@ var board = [
     [1,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,1]
 ];
 
 // var elements_on_board = [
 //     []
 // ]
-
 
 var t=board.length-8;
 
@@ -34,22 +33,41 @@ function draw_board(){
         for(var j=0;j<8;j++){
             if(board[i][j]===0){
                 if(i%2===0&&j===3)
-                    $('.board').append('<div class="board__field board__field_road road-stripe"></div>');
+                    $('.board').append('<div id='+(i-t)+j+' class="board__field board__field_road road-stripe"></div>');
                 else
-                    $('.board').append('<div class="board__field board__field_road"></div>');
+                    $('.board').append('<div id='+(i-t)+j+' class="board__field board__field_road"></div>');
             }
             else
-                $('.board').append('<div class="board__field board__field_grass"></div>');
+                $('.board').append('<div id='+(i-t)+j+' class="board__field board__field_grass"></div>');
         }
     }
+}
+var x=4;
+function draw_player(){
+
+    $('#7'+x).toggleClass('player');
+
+
 }
 
 var board_move = setInterval (function(){
     $('.board').empty();
     draw_board();
-    t--;
-    if(t<0)
-        clearInterval(board_move);
-}, 500);
+    draw_player();
 
+    t--;
+    if(t<0){
+        // clearInterval(board_move);
+        t=board.length-8;
+    }
+
+}, 300);
+$(document).keydown(function(e){
+    if (e.keyCode == 37) {
+        x-=1;
+    }
+    if (e.keyCode == 39) {
+        x+=1;
+    }
+});
 
