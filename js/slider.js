d41afe04047a91d6f0c511fb78403ed92711dd11
Michal Lacki <mlacki@gmail.com>
2016-12-02 16:09:07 +0100

    bootstrap link added

--- js/slider.js
+++ js/slider.js
@@ -13,31 +13,3 @@ function slide () {
 setInterval(slide, 10000);
 
 
-//
-// $("#home").removeClass();
-// $("#home").addClass("slider2");
-//
-// $("#home").removeClass();
-// $("#home").addClass("slider3");
-
-
-
-
-//
-//
-// $(document).ready(function()
-// {
-//     $("#home").click(function()
-//     {
-//
-//         $(this).removeClass();
-//         $(this).addClass("slider")
-//     })
-//     $("#prova.show").click(function()
-//     {
-//
-//         $(this).removeClass();
-//         $(this).addClass("hide")
-//     })
-//
-// })
