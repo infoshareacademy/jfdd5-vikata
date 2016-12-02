dbb905d539cfbc8d965d401d1026a8ecccac3842
Michal Lacki <mlacki@gmail.com>
2016-12-02 16:08:30 +0100

    Slider works without buttons

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,24 @@
 /**
  * Created by mlacki on 01.12.16.
  */
-
-
-
 $("#home").addClass("slider1");
+var sliderClasses = ["slider1", "slider2", "slider3"];
+var slideId =0;
+function slide () {
+    $("#home").removeClass(sliderClasses[slideId]);
+    slideId = (slideId + 1) % sliderClasses.length;
+    $("#home").addClass(sliderClasses[slideId]);
+}
 
-$("#home").removeClass();
-$("#home").addClass("slider2");
+setInterval(slide, 10000);
 
-$("#home").removeClass();
-$("#home").addClass("slider3");
+
+//
+// $("#home").removeClass();
+// $("#home").addClass("slider2");
+//
+// $("#home").removeClass();
+// $("#home").addClass("slider3");
 
 
 
