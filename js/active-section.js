96c12454837038014b538dc7e4ebfc511352ce1e
mlacki <mlacki@gmail.com>
2016-12-08 23:25:44 +0100

    menu improved

--- js/active-section.js
+++ js/active-section.js
@@ -6,23 +6,15 @@
 $(window).scroll(function(){
   var position = $(this).scrollTop();
 
-    $('section').each(function () {
+     $('section').each(function () {
       var target = $(this).offset().top - 500;
       var id = $(this).attr('id');
 
-
-
      if (position >= target) {
-       // console.log('podswietlam ID', id)
         $('ul.nav li').removeClass('active-element');
         $('ul.nav li > a[href="#' + id + '"]').parent().addClass('active-element');
-
-
       }
-
     }
   )
-
-
 }
 )
